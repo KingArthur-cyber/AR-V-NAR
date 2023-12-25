c7fed69ecf914145aaae0e03645735a00c1dc2f6
KingArthur-cyber <112247221+KingArthur-cyber@users.noreply.github.com>
2023-12-25 14:23:13 +0530

    Update blast.py

--- blast.py
+++ blast.py
@@ -1,3 +1,21 @@
+import time
+st = time.time()
+import numpy as np
+def reverse_complement(seq):
+    rev_comp_seq = ''
+    seq = seq[::-1]
+    for i in range(0,len(seq)):
+        if seq[i] == 'A':
+            rev_comp_seq += 'T'
+        elif seq[i] == 'T':
+            rev_comp_seq += 'A'
+        elif seq[i] == 'G':
+            rev_comp_seq += 'C'
+        else:
+            rev_comp_seq += 'G'
+    return rev_comp_seq
+    
+        
 def fasta_reader(file_name):
     with open(file_name) as f:
         new_dict = {}
@@ -18,58 +36,114 @@ def fasta_reader(file_name):
     for values in new_dict.values():
         value.append(values)
     return value
+    
+        
+def edit_dist(query,seq):
+    matrix = np.zeros((len(query)+1,len(seq)+1))
+    for i in range(0,len(seq)+1):
+        matrix[i][0] = i 
+        matrix[0][i] = i 
+    for i in range(1,len(seq)+1):
+        for j in range(1,len(query)+1):
+            left = matrix[i][j-1]
+            right =  matrix[i-1][j]
+            diag = matrix[i-1][j-1]
+            minim  =  min(left,right,diag)
+            if seq[i-1] == query[j-1]:
+                matrix[i][j] = minim
+            elif seq[i-1] != query[j-1]:
+                matrix[i][j] = minim + 1
+    return matrix[len(seq)][len(query)]
 
 def kmer(query,w=11):
-    mapu = {}
+    hash_map = {}
     for i in range(0,len(query)-w+1):
         sub_seq = query[i:i+w]
-        if sub_seq in mapu:
-            mapu[sub_seq].append(i)
+        if sub_seq in hash_map:
+            hash_map[sub_seq].append(i)
         else:
-            mapu[sub_seq] = [i]
-    return mapu
-
-def get_hits(seq,m,w=11):
-    res = []
-    for i in range(0,len(seq)-w+1):
-        sub_seq = seq[i:i+w]
-        if sub_seq in m:
-            l = m[sub_seq]
+            hash_map[sub_seq] = [i]
+    return hash_map
+def hits(seq,query_map,w=11):
+    k_index = []
+    for i in range(0,len(seq)- w + 1):
+        seq_mer = seq[i:i+w]
+        if seq_mer in query_map:
+            l = query_map[seq_mer]
+            #print(l)
             for ind in l:
-                res.append((ind,i))
-    return res
+                k_index.append((ind,i))
+    return k_index
     
-def extend_hits(seq,hit,query,w=11)
-    stq,sts = hit[0],hit[1]
-    matfw = 0
-    k = 0
-    bestk = 0
-    while 2*matfw >= k and stq + w + k < len(query) and sts + w + k < len(seq):
-        if query[sts+w+k] == seq[sts+w+k]:
-            matfw += 1
-            bestk = k + 1
-        k += 1
-    size = w + bestk
-    k = 0
-    matbw = 0
-    bestk = 0
-    while 2*matbw >= k and stq > k and sts > k:
-        if query[stq-k-1] == seq[sts-k-1]:
-            matbw +=1 
-            bestk = k+1
-        k+=1
-    size += bestk
-    return (stq-bestk,sts-bestk,size,w+matfw+matbw)
+def hamming_dist(seq,query):
+    count = 0
+    length = len(query)
+    for i in range(0,len(seq)):
+        if seq[i] == query[i]:
+            pass
+        elif seq[i] != query[i]:
+            count += 1
+    percent = (count/length) * 100
+    return percent
     
-def hit_best_score(seq,query,m,w):
-    hits = get_hits(seq,m,w)
-    best_score = -1.0
-    for h in hits:
-        ext = extends_hit(seq,h,query,w)
-        score = ext[3]
-        if score > best_score or
-seq = fasta_reader('sequence.fasta')[0]
-query = fasta_reader('sequence.fasta')[1]
-m = kmer(query,11)
-get_hit = get_hits(seq,m,11)
-print(get_hit)
+def extend_hits(query,seq,indexes,w=11):
+    index_q = []
+    index_s = []
+    answers = []
+    for i in range(0,len(indexes)):
+        index_q.append(indexes[i][0])
+        index_s.append(indexes[i][1])
+    
+    for i in range(0,len(index_q)):
+        index_f_q = index_q[i]
+        index_b_q = index_q[i]
+        
+        index_f_s = index_s[i]
+        index_b_s = index_s[i]
+        score = 0
+        count = 0
+        threshold_score = 95
+        #print(i)
+        if index_q[i] >-1:
+            while (index_f_q < (len(query)-1) and index_f_s < (len(seq)-1)) and (score < threshold_score):
+                score = hamming_dist(seq[index_b_s:index_f_s+1],query[index_b_q:index_f_q+1])
+                
+                index_f_q += 1
+                index_f_s += 1
+                if index_b_q > 0 and index_b_s > 0:
+                    index_b_q -= 1
+                    index_b_s -= 1
+                elif index_b_q <= 0:
+                    index_b_q = 0
+                if index_b_s <= 0:
+                    index_b_s = 0
+                
+                count += 1
+                #print(count)
+        if (score < 95) and (abs(index_q[i] - index_f_q) > 600):
+            answers.append((index_q[i],index_s[i],index_f_q,index_f_s))
+    return answers
+            
+       
+            
+            
+        
+        
+            
+            
+db_seq = fasta_reader('sequence.fasta')[0]
+rev_seq = reverse_complement(db_seq)
+query = fasta_reader('fusb.fasta')[0]
+
+
+hash_map = kmer(query)
+#print(db_seq)
+
+#print(len(hits(db_seq,hash_map)))
+hitu = hits(rev_seq,hash_map)
+print(len(rev_seq))
+print(hitu)
+print(extend_hits(query,rev_seq,hitu))
+et = time.time()
+elapsed_time = et-st
+print("Execution time: ",elapsed_time,"seconds")
